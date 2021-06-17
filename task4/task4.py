534ce96b261e10284b004273f4e408bcbb5ed675
Leolightth <leolightth@gmail.com>
2021-06-17 22:16:29 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -5,6 +5,9 @@ import re
 
 
 def create_pattern(string_2: str='*'):
+    '''
+    Создание паттерна для поиска из заданной строки
+    '''
     pattern = ''
     for symbol in string_2:
         if symbol == '*':
@@ -17,12 +20,16 @@ def create_pattern(string_2: str='*'):
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        #sys.argv[0] - имя программы
-
+        # получение параметров консоли
+        # первая строка
         string_1 = sys.argv[1]
+        # вторая строка
         string_2 = sys.argv[2]
+        # создаем паттерн для сравнения из второй строки
         regex_string = create_pattern(string_2)
+        # сравниваем первую строку с паттерном, созданным из второй
         match = regex_string.match(string_1)
+        # в зависимости от наличия соответствия делаем пишем 'OK' или 'KO'
         if match:
             print('OK')
         else:
